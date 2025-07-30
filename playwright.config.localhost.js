d8d1884492c02e7ccefae021e2106a57a1226e48
Garry Rogers <garry.rogers@titanium.solutions>
2025-07-30 17:49:03 +0100

    Update baseURL in Playwright config to use localhost for local testing

--- playwright.config.localhost.js
+++ playwright.config.localhost.js
@@ -19,7 +19,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL || 'https://rogersg17.github.io/demoApp/',
+    baseURL: 'http://localhost:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
